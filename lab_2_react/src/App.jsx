0e6dccbd2a77cb4191bb5605942838b74e1b3c2f
Aleck <klymash.sasha@gmail.com>
2026-02-16 20:37:42 +0200

    Recreated CV using React components

---
+++ lab_2_react/src/App.jsx
@@ -0,0 +1,25 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About_me from './components/About_me';
+import Education from './components/Education';
+import Experience from './components/Experience';
+import Languages from './components/Languages';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <About_me />
+      <Education />
+      <Experience />
+      <Skills />
+      <Projects />
+      <Languages />
+      <Footer />
+    </div>
+  )
+}
+export default App;
